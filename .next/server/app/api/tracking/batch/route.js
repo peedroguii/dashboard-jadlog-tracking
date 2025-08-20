4148515ae094625ed834d64f3d0744953b7caa7e
peedroguii <p.guilhermeealves@gmail.com>
2025-08-19 21:05:54 -0300

    Add .next/server/app/api/tracking/batch/route.js 1755648397225

---
+++ .next/server/app/api/tracking/batch/route.js
@@ -0,0 +1,7 @@
+const CHUNK_PUBLIC_PATH = "server/app/api/tracking/batch/route.js";
+const runtime = require("../../../../chunks/[turbopack]_runtime.js");
+runtime.loadChunk("server/chunks/node_modules_next_209863d1._.js");
+runtime.loadChunk("server/chunks/[root-of-the-server]__60b92f37._.js");
+runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/api/tracking/batch/route/actions.js [app-rsc] (server actions loader, ecmascript)", CHUNK_PUBLIC_PATH);
+runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/tracking/batch/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH);
+module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/tracking/batch/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
